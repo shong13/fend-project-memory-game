c514c7bc5bc75e4d3473fc34046630b021fadfb8
Sung Jun Hong <sung.j.hong93@gmail.com>
2018-04-26 18:40:35 -0700

    Adds a list of cards

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 /*
  * Create a list that holds all of your cards
  */
+let cardsFace = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-anchor', 'fa-leaf', 'fa-bicycle', 'fa-diamond', 'fa-bomb', 'fa-leaf', 'fa-bomb', 'fa-bolt', 'fa-bicycle', 'fa-paper-plane-o', 'fa-cube'];
+
 
 
 /*
